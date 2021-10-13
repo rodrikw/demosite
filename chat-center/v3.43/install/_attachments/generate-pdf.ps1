bf9a6ef58a10f3f791ca4e1408dce31ec7636058
rodrikw <rodrik.wade@gmail.com>
2021-10-13 14:44:15 +0200

    Published Chat-Center v3.43.

---
+++ chat-center/v3.43/install/_attachments/generate-pdf.ps1
@@ -0,0 +1,14 @@
+﻿# Retrieve the line in index.adoc starting with :filenamestem:
+$FileName = Get-Content ../pages/index.adoc | Where-Object { $_.Contains(":filenamestem:") }
+# Trim off :filenamestem: from the start of the line
+$FileName = $FileName.TrimStart(":filenamestem: ")
+# Retrieve the system date and format it
+$DateToday = Get-Date -Format "yyyy-MM-dd"
+# Add todays's date and the file extension
+$FileName = $FileName + $DateToday + ".pdf"
+# Retrieve the system date again and format it for the front cover
+$DateToday = Get-Date -Format "d MMMM yyyy"
+# Run Asciidoctor PDF passing the system date, filename and company. Redirect stderr output to errors.txt. This also ensures that the script waits to open the PDF.
+& asciidoctor-pdf -r asciidoctor-diagram ../pages/index.adoc -D . -o $FileName -a docdate=$DateToday -a company="dena" -a filename=$FileName -a compress 2>errors.txt
+# Open the PDF in Foxit PhantomPDF
+& "C:\Program Files (x86)\Foxit Software\Foxit PhantomPDF\FoxitPhantomPDF.exe" $FileName /A zoom=73%
--- start-http-server.ps1
+++
@@ -1,2 +0,0 @@
-# Run the server, disabling caching:
-& http-server . -c-1
