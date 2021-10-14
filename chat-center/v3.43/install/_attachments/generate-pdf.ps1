611339588d133479d07317eb498a8eb23ad9a3ff
rodrikw <rodrik.wade@gmail.com>
2021-10-14 11:51:18 +0200

    Corrected PDF file name

--- chat-center/v3.43/install/_attachments/generate-pdf.ps1
+++ chat-center/v3.43/install/_attachments/generate-pdf.ps1
@@ -3,10 +3,10 @@ $FileName = Get-Content ../pages/index.adoc | Where-Object { $_.Contains(":filen
 # Trim off :filenamestem: from the start of the line
 $FileName = $FileName.TrimStart(":filenamestem: ")
 # Retrieve the system date and format it
-$DateToday = Get-Date -Format "yyyy-MM-dd"
-# Add todays's date and the file extension
-$FileName = $FileName + $DateToday + ".pdf"
-# Retrieve the system date again and format it for the front cover
+# $DateToday = Get-Date -Format "yyyy-MM-dd"
+# Add the file extension
+$FileName = $FileName + ".pdf"
+# Retrieve the system date and format it for the front cover
 $DateToday = Get-Date -Format "d MMMM yyyy"
 # Run Asciidoctor PDF passing the system date, filename and company. Redirect stderr output to errors.txt. This also ensures that the script waits to open the PDF.
 & asciidoctor-pdf -r asciidoctor-diagram ../pages/index.adoc -D . -o $FileName -a docdate=$DateToday -a company="dena" -a filename=$FileName -a compress 2>errors.txt
